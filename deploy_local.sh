935a60386a7f3f2720d2f36bd66271331af91583
agustin <agustin@A796LSL.tcpsi.es>
2020-04-10 12:25:24 +0200

    add dir to gitgnore

---
+++ deploy_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker stop pystol-client
+echo  "docker stop pystol-client"
+docker rm  pystol-client
+echo  "docker rm  pystol-client"
+docker rmi pystol-client
+docker build -f Dev/Dockerfile -t "pystol-client" .
+docker images 
+docker run -d -it \
+-e KUBECONFIG=".kube/config" \
+-p 3000:3000  \
+-v pystol_log:/var/log/  \
+-v c:/Users/agustin/Desktop/codigos/Pystol-client-in-docker/pystol-ui:/home \
+--name pystol-client pystol-client:latest
+docker cp kubeconfig/kubeconfig.yml  pystol-client:.kube/config
+docker exec -it pystol-client sh -c 'cat .kube/config'
+docker exec -it pystol-client sh -c 'env'
+docker exec -it pystol-client sh -c 'kubectl cluster-info'
+docker exec -it pystol-client sh -c 'flask run --host=0.0.0.0 --port=3000'
